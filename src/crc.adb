714661f7dd1c87d2051055bc70b5d154cdfea807
osannolik <jonas@godtycklig.se>
2017-09-05 21:54:30 +0200

    Crc 8 package

---
+++ src/crc.adb
@@ -0,0 +1,28 @@
+with HAL; use HAL;
+
+package body CRC is
+
+   function Calculate (Crc  : in AMC_Types.UInt8;
+                       Data : in AMC_Types.Byte_Array)
+                       return UInt8 is
+      Tmp : AMC_Types.UInt8 := Crc;
+   begin
+      for D of Data loop
+         Tmp := Crc8_Table (Table_Index (D xor Tmp));
+      end loop;
+
+      return Tmp;
+   end Calculate;
+
+   function Calculate (Data : in AMC_Types.Byte_Array)
+                       return AMC_Types.UInt8 is
+      Tmp : AMC_Types.UInt8 := Crc8_Init;
+   begin
+      for D of Data loop
+         Tmp := Crc8_Table (Table_Index (D xor Tmp));
+      end loop;
+
+      return Tmp;
+   end Calculate;
+
+end CRC;
