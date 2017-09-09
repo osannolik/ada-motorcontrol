89b0532d5dd32ce04d43790d92c910c91db42a19
osannolik <jonas@godtycklig.se>
2017-09-09 15:37:58 +0200

    Write byte array to memory

--- src/communication.adb
+++ src/communication.adb
@@ -24,6 +24,11 @@ package body Communication is
    procedure Commands_Send_Error (Port                     : in out Port_Type;
                                   Causing_Interface_Number : in Interface_Number_Type);
 
+   procedure Commands_Write_To (Data  : access Byte_Array;
+                                Error : out Boolean);
+
+
+
    procedure Initialize (Interface_Obj    : access Interface_Type'Class;
                          Interface_Number : in Interface_Numbers) is
    begin
@@ -122,20 +127,79 @@ package body Communication is
    end Commands_Send_Error;
 
 
+   procedure Commands_Write_To (Data  : access Byte_Array;
+                                Error : out Boolean) is
+
+      type Commands_Write_To_Cmd is record
+         To_Address : access Byte_Array := null;
+         Length     : AMC_Types.UInt16 := 16#0#;
+      end record
+         with Size => 48;
+
+      for Commands_Write_To_Cmd use record
+         To_Address at 0 range 0  .. 31;
+         Length     at 0 range 32 .. 47;
+      end record;
+
+      type Commands_Write_To_Type
+         (As_Array : Boolean := False)
+      is record
+         case As_Array is
+            when False =>
+               Cmd : Commands_Write_To_Cmd;
+
+            when True =>
+               Arr : Byte_Array (0 .. 5);
+
+         end case;
+      end record
+         with Unchecked_Union, Size => 48;
+
+      Write  : Commands_Write_To_Type;
+      Cmd_Length : constant Natural := Commands_Write_To_Cmd'Size / 8;
+   begin
+      Error := True;
+      if Data'Length >= Cmd_Length then
+         Write := Commands_Write_To_Type'(As_Array => True,
+                                          Arr      => Data (0 .. Cmd_Length - 1));
+         if Natural (Write.Cmd.Length) = Data'Length - Cmd_Length then
+            declare
+               To_Ref : constant access Byte_Array := Write.Cmd.To_Address;
+               I : Natural := 0;
+            begin
+               for D of Data (Cmd_Length .. Data'Length - 1) loop
+                  To_Ref (I) := D;
+                  I := I + 1;
+               end loop;
+            end;
+            Error := False;
+         end if;
+      end if;
+   end Commands_Write_To;
+
+
    procedure Commands_Callback_Handler (Identifier : in Identifier_Type;
                                         Data       : access Byte_Array) is
-      pragma Unreferenced (Data);
+      Error : Boolean;
    begin
       case Identifier is
          when Com_Id_Write_To =>
-            null;
+            Commands_Write_To (Data  => Data,
+                               Error => Error);
+
          when Com_Id_Read_From =>
             null;
+
          when Com_Id_Error =>
             null;
+
          when others =>
             null;
       end case;
+--        if Error then
+--           --  TODO: Do something
+--           null;
+--        end if;
    end Commands_Callback_Handler;
 
 
