3cda22e9fb35971c7abe0540d8ace365740c0350
osannolik <jonas@godtycklig.se>
2017-10-26 21:59:08 +0200

    Cumulative average

---
+++ src/amc_utils-cum_avg.adb
@@ -0,0 +1,25 @@
+package body AMC_Utils.Cum_Avg is
+
+   procedure Add (C_Avg : in out Cumulative_Average;
+                  Value : in     Element_T)
+   is
+      N : constant Natural := Natural'Succ (C_Avg.N);
+   begin
+      C_Avg := Cumulative_Average'
+         (Mean => C_Avg.Mean + (Value - C_Avg.Mean) / Element_T (N),
+          N    => N);
+   end Add;
+
+   procedure Set (C_Avg : in out Cumulative_Average;
+                  Value : in     Element_T)
+   is
+   begin
+      C_Avg := Cumulative_Average'
+         (Mean => Value,
+          N    => 1);
+   end Set;
+
+   function Get (C_Avg : in Cumulative_Average) return Element_T is
+      (C_Avg.Mean);
+
+end AMC_Utils.Cum_Avg;
---
+++ src/amc_utils-cum_avg.ads
@@ -0,0 +1,23 @@
+generic
+
+   Max : Positive;
+
+   type Element_T is digits <>;
+
+package AMC_Utils.Cum_Avg is
+
+   type Cumulative_Average is tagged record
+      Mean : Element_T := 0.0;
+      N    : Natural range Natural'First .. Max := Natural'First;
+   end record;
+
+   procedure Add (C_Avg : in out Cumulative_Average;
+                  Value : in     Element_T);
+
+   procedure Set (C_Avg : in out Cumulative_Average;
+                  Value : in     Element_T);
+
+   function Get (C_Avg : in Cumulative_Average)
+                 return Element_T;
+
+end AMC_Utils.Cum_Avg;
