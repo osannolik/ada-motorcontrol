45caf0b0bc592e6a3e15acf5b4876d541a74ca12
osannolik <jonas@godtycklig.se>
2017-10-27 18:26:31 +0200

    Make private

--- src/amc_utils-cum_avg.ads
+++ src/amc_utils-cum_avg.ads
@@ -4,10 +4,7 @@ generic
 
 package AMC_Utils.Cum_Avg is
 
-   type Cumulative_Average is tagged record
-      Mean : Element_T := 0.0;
-      N    : Natural   := Natural'First;
-   end record;
+   type Cumulative_Average is tagged private;
 
    procedure Add (C_Avg : in out Cumulative_Average;
                   Value : in     Element_T);
@@ -17,5 +14,12 @@ package AMC_Utils.Cum_Avg is
 
    function Get (C_Avg : in Cumulative_Average)
                  return Element_T;
+
+private
+
+   type Cumulative_Average is tagged record
+      Mean : Element_T := 0.0;
+      N    : Natural   := Natural'First;
+   end record;
 
 end AMC_Utils.Cum_Avg;
